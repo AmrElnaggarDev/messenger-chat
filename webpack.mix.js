4a3a150259847e0294cf4543c2fc5f5bfe466be7
Amr Elnaggar <UG_31202189@ics.tanta.edu.eg>
2026-02-09 22:58:35 +0200

    feat(ui): migrate messenger view to Vue components

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,9 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/messages.js', 'public/js')
+    .vue({ version: 3 })
+    .extract(['vue'])
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
     ]);
